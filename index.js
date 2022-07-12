781907f4cb07e2e46d308b1eaac34747d1375b35
Xinyue (Luna) <xinyuec57@gmail.com>
2022-07-12 18:12:17 -0400

    make it better

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static('./client/build'));
 }
 
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+})
+
 // configure session
 app.use(
   session({
