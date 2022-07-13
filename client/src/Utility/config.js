5bea851aee62cc0c6178dbc50fe4c04fe20a4500
Xinyue (Luna) <xinyuec57@gmail.com>
2022-07-13 14:47:48 -0400

    make it better

--- client/src/Utility/config.js
+++ client/src/Utility/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://mern-e-commerce-first-app-1.herokuapp.com/"
+  baseURL: "https://mern-e-commerce-first-app-1.herokuapp.com"
 })
