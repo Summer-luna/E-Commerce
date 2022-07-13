1efbf34a581a1a73b805cec9bf06748b29e1ef81
Xinyue (Luna) <xinyuec57@gmail.com>
2022-07-13 15:33:10 -0400

    make it better

--- index.js
+++ index.js
@@ -128,9 +128,9 @@ app.post("/login", (req, res)=>{
 
 app.get("/auth/google", passport.authenticate('google', { scope: ['profile'] }));
 
-app.get("/auth/google/account", passport.authenticate('google', {failureRedirect: "http://localhost:3000/login"}),(req, res)=>{
+app.get("/auth/google/account", passport.authenticate('google', {failureRedirect: "/login"}),(req, res)=>{
   // successful authentication, response to client
-  console.log(res);
+  //console.log(res);
   res.redirect("/");
 });
 
