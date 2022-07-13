d7ed1021809efe5e6487c1f80554af1e536ca793
Xinyue (Luna) <xinyuec57@gmail.com>
2022-07-13 18:04:36 -0400

    make it better

--- client/src/Context/ShoppingCartContext.jsx
+++ client/src/Context/ShoppingCartContext.jsx
@@ -49,8 +49,6 @@ export const ShoppingCartProvider = ({children}) => {
       if(isAuth){
         const { data } = await axios.get("/getCart");
         setCartItems(data.data);
-      }else {
-        setCartItems(null);
       }
     }
     getCart();
