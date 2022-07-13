47ca157609f79c1f836c566ed875c1e15bc78656
Xinyue (Luna) <xinyuec57@gmail.com>
2022-07-13 15:08:37 -0400

    make it better

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static('./client/build'));
 }
 
-/*app.get("*", (req, res)=>{
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-})*/
-
 // configure session
 app.use(
   session({
@@ -306,6 +302,10 @@ app.get("/getOrders", (req, res)=>{
     }
   });
 });
+
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
